f6267a94140b20563ada948fe00ca1eaea0f73b4
T.Hashimoto <tkrhsmt@gmail.com>
2024-08-25 16:39:21 +0900

    add adams-bashforth method

--- src/CPU_2D/main.jl
+++ src/CPU_2D/main.jl
@@ -27,15 +27,25 @@ prm = inputfile_param(INPUT_FILE)
 # old velocity (x and y direction)
 ux1 = zeros(prm.nx + 3, prm.ny + 2)
 uy1 = zeros(prm.nx + 2, prm.ny + 3)
-# new velocity (x and y direction)
+# intermidiate velocity (x and y direction)
 ux2 = zeros(prm.nx + 3, prm.ny + 2)
 uy2 = zeros(prm.nx + 2, prm.ny + 3)
+# new velocity (x and y direction)
+ux3 = zeros(prm.nx + 3, prm.ny + 2)
+uy3 = zeros(prm.nx + 2, prm.ny + 3)
 # old pressure
 pp1 = zeros(prm.nx + 2, prm.ny + 2)
 # new pressure
 pp2 = zeros(prm.nx + 2, prm.ny + 2)
 # velocity divergence
 div = zeros(prm.nx + 2, prm.ny + 2)
+# adams-bashforth
+fx1 = zeros(prm.nx + 3, prm.ny + 2)
+fy1 = zeros(prm.nx + 2, prm.ny + 3)
+fx2 = zeros(prm.nx + 3, prm.ny + 2)
+fy2 = zeros(prm.nx + 2, prm.ny + 3)
+fx3 = zeros(prm.nx + 3, prm.ny + 2)
+fy3 = zeros(prm.nx + 2, prm.ny + 3)
 
 # ---------- setting folder ---------- #
 # if there is no "data" folfer, create it
@@ -45,14 +55,15 @@ end
 
 # ---------- main program ---------- #
 
-function MAIN_PROGRAM(prm)
+function MAIN_PROGRAM(prm, ux1, uy1, ux2, uy2, ux3, uy3, pp1, pp2, div, fx1, fx2, fx3, fy1, fy2, fy3)
 
     # initial condition
     init!(ux1, uy1, prm)
     # initial log
     init_log(prm)
 
-    for time in prm.istart:prm.iend
+    # first 2 step is euler method
+    for time in prm.istart:prm.istart+1
 
         # 1st FS step (ux1, uy1) -> (ux2, uy2)
         first_velocity!(ux1, uy1, ux2, uy2, prm)
@@ -60,8 +71,51 @@ function MAIN_PROGRAM(prm)
         # poisson equation pp1 -> pp1
         rep = poisson!(ux2, uy2, pp1, pp2, div, prm)
 
-        # 2nd FS step (ux2, uy2) -> (ux1, uy1)
-        second_velocity!(ux2, uy2, ux1, uy1, pp1, prm)
+        # 2nd FS step (ux2, uy2) -> (ux3, uy3)
+        second_velocity!(ux2, uy2, ux3, uy3, pp1, prm)
+
+        # preserve adams-bashforth data
+        if time == 1
+            fx3 = (ux3 - ux1) / prm.dt
+            fy3 = (uy3 - uy1) / prm.dt
+        elseif time == 2
+            fx2 = (ux3 - ux1) / prm.dt
+            fy2 = (uy3 - uy1) / prm.dt
+        end
+
+        # return next step first data
+        ux1 = copy(ux3)
+        uy1 = copy(uy3)
+
+    end
+
+    for time in prm.istart+2:prm.iend
+
+        # 1st FS step (ux1, uy1) -> (ux2, uy2)
+        first_velocity!(ux1, uy1, ux2, uy2, prm)
+
+        # poisson equation pp1 -> pp1
+        rep = poisson!(ux2, uy2, pp1, pp2, div, prm)
+
+        # 2nd FS step (ux2, uy2) -> (ux3, uy3)
+        second_velocity!(ux2, uy2, ux3, uy3, pp1, prm)
+
+        # adams-bashforth data 1
+        fx1 = (ux3 - ux1) / prm.dt
+        fy1 = (uy3 - uy1) / prm.dt
+
+        # return next step first data (adams-bashforth method)
+        #ux1 = ux1 + prm.dt * (23/12 * fx1 - 16/12 * fx2 + 5/12 * fx3)
+        #uy1 = uy1 + prm.dt * (23/12 * fy1 - 16/12 * fy2 + 5/12 * fy3)
+        ux1 = ux1 + prm.dt * (3 / 2 * fx1 - 1 / 2 * fx2)
+        uy1 = uy1 + prm.dt * (3 / 2 * fy1 - 1 / 2 * fy2)
+
+        # adams-bashforth data 3
+        fx3 = copy(fx2)
+        fy3 = copy(fy2)
+        # adams-bashforth data 2
+        fx2 = copy(fx1)
+        fy2 = copy(fy1)
 
         # output log
         if time % prm.ilog == 0
@@ -82,4 +136,4 @@ function MAIN_PROGRAM(prm)
 
 end
 
-MAIN_PROGRAM(prm)
+MAIN_PROGRAM(prm, ux1, uy1, ux2, uy2, ux3, uy3, pp1, pp2, div, fx1, fx2, fx3, fy1, fy2, fy3)
